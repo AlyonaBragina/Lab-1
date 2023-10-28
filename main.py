0dae110b22a3eebdd53c5839ea99193c688f6c5d
sphrl <alenabragina2000@gmail.com>
2023-10-28 17:35:15 +0300

    COMPLETE 4

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
+#TASK1
 # RED = '\u001b[41m'
-# BLUE = '\u001b[44m'
 # WHITE = '\u001b[47m'
 # END = '\u001b[0m'
+# for i in range(10):
+#     if i<2 or i>7:
+#         print(f'{RED}{"  " * (14)}{END}')
+#     elif (i>1 and i<4) or (i>5 and i<8):
+#         print(f'{RED}{"  " * (6)}{WHITE}{"  " * (2)}{RED}{"  " * (6)}{END}')
+#     elif i>3 and i<6:
+#         print(f'{RED}{"  " * (4)}{WHITE}{"  " * (6)}{RED}{"  " * (4)}{END}')
+#     
 
-# for i in range(6):
-#     if i < 3:
-#         print(f'{BLUE}{"  " * (2 * i + 2)}{WHITE}{"  " * (14 - 2 * i)}{END}')
-#     else:
-#         print(f'{BLUE}{"  " * (12 - 2 * i)}{RED}{"  " * (4 + 2 * i)}{END}')
-
-RED = '\u001b[41m'
+#TASK2
+BLUE = '\u001b[44m'
 WHITE = '\u001b[47m'
 END = '\u001b[0m'
-for i in range(10):
-    if i<2:
-        print(f'{RED}{"  " * (14)}{END}')
-    elif i>1 and i<4:
-        print(f'{RED}{"  " * (6)}{WHITE}{"  " * (2)}{RED}{"  " * (6)}{END}')
-    elif i>3 and i<6:
-        print(f'{RED}{"  " * (4)}{WHITE}{"  " * (6)}{RED}{"  " * (4)}{END}')
-    elif i>5 and i<8:
-        print(f'{RED}{"  " * (6)}{WHITE}{"  " * (2)}{RED}{"  " * (6)}{END}')
-    else:
-        print(f'{RED}{"  " * (14)}{END}')
-    
+for i in range(4):
+    if i==0 or i==3:
+        print(f'{WHITE}{"  " * (2)}{BLUE}{"  " * (2)}{WHITE}{"  " * (1)}{BLUE}{"  " * (2)}{WHITE}{"  " * (2)}{END}')
+    elif i==1 or i==2:
+        print(f'{WHITE}{"  " * (1)}{BLUE}{"  " * (1)}{WHITE}{"  " * (2)}{BLUE}{"  " * (1)}{WHITE}{"  " * (2)}{BLUE}{"  " * (1)}{WHITE}{"  " * (1)}{END}')
 
 # plot_list = [[0 for i in range(10)] for i in range(10)]
 # result = [0 for i in range(10)]
