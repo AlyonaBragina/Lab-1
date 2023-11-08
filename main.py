dfbbec2e4a2ae00b3def7a7a702f06d86bad0cec
sphrl <alenabragina2000@gmail.com>
2023-11-08 16:19:46 +0300

    1

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ listnew=[]
 for number in file:
     lst.append(float(number))
 for i in range(len(lst)):
-    if int(lst[i])>-3 and  int(lst[i])<3:
+    if lst[i]>-3 and  lst[i]<3:
         listnew.append(i)
 file.close()
 print(listnew)
